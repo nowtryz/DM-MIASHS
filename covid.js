5c6e3ed90d8828c12e44d6486fd8ee448728138f
Nowtryz <nowtryz.dev@gmail.com>
2021-01-03 20:22:28 +0100

    fix css

---
+++ covid.js
@@ -0,0 +1,92 @@
+console.error("covid")
+
+var fr = document.getElementById("mapFrance")
+fr.addEventListener("click", function(){
+    var x = document.getElementById("card_fr")
+    x.style.display = "block"
+})
+
+
+
+var fr = document.getElementById("closeFrance")
+
+fr.addEventListener("click", function(e){  
+e.preventDefault();
+showCard(this.card_covid); 
+	
+
+function showCardfrance(("card_covid") {
+	document.getElementById("card_fr").style.display = "none"
+}
+
+var be = document.getElementById("mapBelgique")
+ 	fr.addEventListener("click", 2)
+
+ 	function 2(){
+ 		var x = document.getElementById("card_be")
+ 		x.style.display = "block"
+ 	}
+
+ 	var be = document.getElementById("closeBelgique")
+	  be.addEventListener("click", function(e2){  
+	   e2.preventDefault();    
+	   showCard(this.card_covid); 
+	
+
+function showCardbelgique(("card_covid") {
+	document.getElementById("card_be").style.display = "none"
+}
+
+var it = document.getElementById("mapItalie")
+ 	it.addEventListener("click", 3)
+
+ 	function 3(){
+ 		var x = document.getElementById("card_it")
+ 		x.style.display = "block"
+ 	}
+
+ 	var it = document.getElementById("closeItalie")
+	  it.addEventListener("click", function(e3){  
+	   e3.preventDefault();    
+	   showCard(this.card_covid); 
+	
+
+function showCardfrance(("card_covid") {
+	document.getElementById("card_it").style.display = "none"
+}
+
+var es = document.getElementById("mapEspagne")
+ 	es.addEventListener("click", 4)
+
+ 	function 4(){
+ 		var x = document.getElementById("card_es")
+ 		x.style.display = "block"
+ 	}
+
+ 	var es = document.getElementById("closeEspagne")
+	  es.addEventListener("click", function(e4){  
+	   e4.preventDefault();    
+	   showCard(this.card_covid); 
+	
+
+function showCardEspagne(("card_covid") {
+	document.getElementById("card_es").style.display = "none"
+}
+
+var hu = document.getElementById("mapHongrie")
+ 	hu.addEventListener("click", 5)
+
+ 	function 5(){
+ 		var x = document.getElementById("card_hu")
+ 		x.style.display = "block"
+ 	}
+
+ 	var hu = document.getElementById("closeHongrie")
+	  hu.addEventListener("click", function(e5){  
+	   e5.preventDefault();    
+	   showCard(this.card_covid); 
+	
+
+function showCardHongrie(("card_covid") {
+	document.getElementById("card_hu").style.display = "none"
+}
